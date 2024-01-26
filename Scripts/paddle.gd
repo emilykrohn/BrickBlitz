22bba38b09f7b356327efdeebd334f9e102d231a
Emily Krohn <emilykrohn2@gmail.com>
2024-01-25 20:52:12 -0800

    Change paddle controls
    Uses A and D to move left and right instead of left and right arrow keys

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
