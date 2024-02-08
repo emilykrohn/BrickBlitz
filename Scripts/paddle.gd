1268fc363fcab3216711ee7f3ea12f221c946bf2
Emily Krohn <emilykrohn2@gmail.com>
2024-02-07 21:28:44 -0800

    Stop paddle from going off screen
    Added an Area2D that detects if the left or right line have been hit. It doesn't allow it to move any further in that direction until it moves away from the wall

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -3,8 +3,23 @@ extends AnimatableBody2D
 
 @export var speed: float = 4.0
 
+var can_move_left := true
+var can_move_right := true
+
 func _physics_process(delta):
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and can_move_left:
 		position.x -= speed
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") and can_move_right:
 		position.x += speed
+
+func _on_area_2d_body_entered(body):
+	if body.name == "LeftLine":
+		can_move_left = false
+	elif body.name == "RightLine":
+		can_move_right = false
+
+func _on_area_2d_body_exited(body):
+	if body.name == "LeftLine":
+		can_move_left = true
+	elif body.name == "RightLine":
+		can_move_right = true
